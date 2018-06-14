61246f89aa2dea637e3eff374eaeefe98fb1aabc
diego fontecilla <mail@diegofontecilla.com>
2018-06-14 16:36:33 +0100

    low temperature limit

---
+++ spec/FeatureSpec.js
@@ -0,0 +1,7 @@
+// 'use strict';
+//
+// describe('Feature Test:', function(){
+//   it('thermostat starts at 20 degrees', function(){
+//     expect(thermostat.temperature()).toEqual('20 degrees');
+//   });
+// });
