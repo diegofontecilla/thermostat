98b9272dd3e30ac73f0e63dc45cf8802ba6491cc
diego fontecilla <mail@diegofontecilla.com>
2018-06-16 18:59:47 +0100

    Thermostat: APIs

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,17 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
 
+  $('#current-city').change(function(){
+      var city = $('#current-city').val();
+      var url = 'http://api.openweathermap.org/data/2.5/weather?q='
+      var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed'
+      var units = '&units=metric'
+
+      $.get(url + city + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+      });
+  });
+
   $('#temperature-up').click(function() {
     thermostat.up();
     updateTemperature();
@@ -33,4 +44,8 @@ $(document).ready(function() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
   };
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
 });
